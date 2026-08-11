33c2874514de81b02482dec9e601c1bb540b18d1
sivapalanluxsihan <sivapalanluxsihan@gmail.com>
2026-08-11 08:54:46 +0530

    Initial commit

---
+++ src/main/java/lk/ac/kln/unimart/UnimartBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.kln.unimart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnimartBackendApplication.class, args);
+    }
+
+}
